7e32b1c340a43ccb217461725f37fed33789348c
nojmana <neumannkinga7@gmail.com>
2018-11-11 00:50:08 +0100

    NVM array

--- map/NvmHashMap.hpp
+++ map/NvmHashMap.hpp
@@ -11,6 +11,14 @@
 #include <memory>
 #include <string.h>
 
+#define INTERNAL_MAPS_COUNT 16
+
+template<class V>
+        class Value {
+public:
+    V value;
+};
+
 template<class V>
 class Segment {
 public:
@@ -25,7 +33,8 @@ template<typename V>
 class NvmHashMap {
 private:
 
-    pmem::obj::persistent_ptr < Segment<V> > root;
+    pmem::obj::persistent_ptr<Segment<V> > segments[INTERNAL_MAPS_COUNT];
+    pmem::obj::persistent_ptr<Value<long> > mc[INTERNAL_MAPS_COUNT];
 
     size_t hash(int key) {
         return key;
@@ -37,17 +46,45 @@ public:
     NvmHashMap() {
         std::cout << "NvmHashMap() start" << std::endl;
 
-        this->root = pmem::obj::make_persistent < Segment < V > > ();
+        for (int i = 0; i < INTERNAL_MAPS_COUNT; i++) {
+            this->segments[i] = pmem::obj::make_persistent< Segment<V> >();
+            this->mc[i] = pmem::obj::make_persistent<Value <long> >();
+        }
 
         std::cout << "NvmHashMap() stop" << std::endl;
     }
 
-    int size() {
+    int calcSize(int triesToBlock = 5) {
+        int size = 0;
+        long mc_sum = 0;
+
+        for (int i = 0; i < INTERNAL_MAPS_COUNT; i++) {
+            size++;
+            // TODO: increment by size of segments[i]
+            mc_sum += mc[i]->value;
+        }
+
+        long mc_sum2 = 0;
+        for (int i = 0; i < INTERNAL_MAPS_COUNT; i++) {
+            mc_sum2 += mc[i]->value;
+        }
+
+        if (mc_sum == mc_sum2) {
+            return size;
+        } else {
+            if (triesToBlock > 0) {
+                return calcSize(triesToBlock - 1);
+            } else {
+                throw "Not implemented yet";
+                // put lock on the array and calculate size
+            }
+        }
+
         throw "Not implemented yet!";
     }
 
     void insert(int key, V value) {
-        this->root->value = value;
+        this->segments[key & (INTERNAL_MAPS_COUNT-1)]->value = value;
     }
 
     void insertNew(int key, V value) {
@@ -55,7 +92,7 @@ public:
     }
 
     V get(int key) {
-        return this->root->value;
+        return this->segments[key & (INTERNAL_MAPS_COUNT-1)]->value;
     }
 
     V remove(int key) {
--- map/main.cpp
+++ map/main.cpp
@@ -4,18 +4,36 @@ struct root {
     pmem::obj::persistent_ptr<NvmHashMap<int> > pmap;
 };
 
+bool file_exists(const char *fname) {
+    FILE *file;
+    if ((file = fopen(fname, "r"))) {
+        fclose(file);
+        return true;
+    }
+    return false;
+}
+
 int main(int argc, char *argv[]) {
 
-    std::string path = argv[1];
-
     pmem::obj::pool <root> pop;
+    std::string path = argv[1];
+    bool insertMode = false;
 
-    //pop = pmem::obj::pool<root>::create(path, "", PMEMOBJ_MIN_POOL, (S_IWUSR|S_IRUSR));
+    try {
+        if (!file_exists(path.c_str())) {
+            std::cout << "File doesn't exists, creating pool"<<std::endl;
+            pop = pmem::obj::pool<root>::create(path, "",
+                    PMEMOBJ_MIN_POOL, (S_IWUSR|S_IRUSR));
+            insertMode = true;
+        } else {
+            std::cout << "File exists, opening pool"<<std::endl;
+            pop = pmem::obj::pool<root>::open(path, "");
+        }
+    } catch (pmem::pool_error &e) {
+        std::cerr << e.what() << std::endl;
+        return 1;
+    }
 
-    std::cout << "Log 1"<<std::endl;
-    pop = pmem::obj::pool<root>::open(path, "");
-
-    std::cout << "Log 2"<<std::endl;
     pmem::obj::persistent_ptr <root> root_ptr;
 
     std::cout << "Log 3"<<std::endl;
@@ -28,12 +46,26 @@ int main(int argc, char *argv[]) {
             std::cout << "Log 4a"<<std::endl;
             root_ptr->pmap = pmem::obj::make_persistent<NvmHashMap<int> >();
         });
+        std::cout << "Log 5a"<<std::endl;
+    } /*else {
+        pmem::obj::transaction::run(pop, [&] {
 
-        std::cout << "Log 5"<<std::endl;
+            std::cout << "Log 4b"<<std::endl;
+            root_ptr->pmap->initialize();
+
+        });
+        std::cout << "Log 5b"<<std::endl;
+    }*/
+
+    if (insertMode) {
+        for (int i = 0; i < 16; i++) {
+            root_ptr->pmap->insert(i, 30+i);
+        }
+        std::cout << "Inserting values to array" << std::endl;
+    }
+    for (int i = 0; i < 16; i++) {
+        std::cout << root_ptr->pmap->get(i) << std::endl;
     }
 
-    //root_ptr->pmap->insert(10, 30);
-
     std::cout << "Log 6"<<std::endl;
-    std::cout << root_ptr->pmap->get(10);
 }
